ae8e832306a1176024e4926b8c92d2cda0f3f7ef
黄之鸣 <zhiminghuang@uvic.ca>
2021-03-29 17:39:04 -0700

    create sender.py

---
+++ edtsender.py
@@ -0,0 +1,39 @@
+
+files = "sadasasdsd"
+socket.socket()
+socket.bind()
+ip
+port
+dddress = (ip,port)
+controltype ={'1':1,'2':0}
+def connection_coontrol_set():
+	print ("1 for reiable connection and 0 for connectionless")
+	ans = input()
+	if ans == 1:
+		connectiontype = 1
+	print ("1 for ")
+	print ("1 for reiability control")
+	#read from inputs
+	return connectiontype, controltype
+
+
+
+def packetgeneration(packettype,controltype,data):
+
+	return bytes
+
+
+def ConnectionCreate(connectiontype, address):
+	success = False
+
+	if success
+		return True
+
+def Data_transmission(controltype, address):
+
+	return True
+
+
+def connection_close(connectiontype, address):
+
+	return True
