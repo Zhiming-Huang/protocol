7f8e604f4cd1f2c17347eb79a7572c4eaa517fd6
yushannn <54758595+yushannn@users.noreply.github.com>
2021-03-30 21:04:50 -0700

    update
    update

---
+++ edtpacket.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import socket
+import struct
+
+
+class edtpacket:
+    def __init__(self,version,connection_type, checksum=0, ack=0, seq = 0, win = 0, control = {'1':0,'2':0,'3':0}):
+          self.version = version
+          self.type = connection_type #1 for control, 2 for ack...
+          self.DAT = data
+          self.ack = ack
+          self.seq = seq
+          self.win = win
+          self.checksum = checksum
+          self.control ={}
+          self.Length = 0
+          self.raw = None
+
+    def packet2bytes(self):
+          if self.type == 0x001: #control packet
+             self.length = 
+             self.raw = struct.pack('!BBBBBLLLL',
+             self.version, 
+             self.length,
+             self.FIN ,
+             self.DAT ,
+             self.ACK ,
+             self.Sequence, 
+             self.Acknowledgement, 
+             self.Window ,
+             self.Length ,
+             )
+
+    def bytes2packet(buffer):
+
--- edtsender.py
+++ edtsender.py
@@ -1,11 +1,47 @@
+import edtpacket
+import threading
 
 files = "sadasasdsd"
 socket.socket()
 socket.bind()
-ip
-port
+
 dddress = (ip,port)
 controltype ={'1':1,'2':0}
+txbuffer = [] # Keeps data sent by application but not acknowledged by peer yet
+rxbuffer = [] # Keeps data received from peer and not received by application yet
+
+
+fsmstate = "CLOSED" #initially the connection is closed, 
+#"SND_CONNECTED" for single-directed connection from sender to the receiver (i.e., sender can send data to the receiver)
+
+
+# Receiving window parameters
+rcv_ini = 1  # Initial ack number
+rcv_nxt = None # Next seq to be received
+rcv_wnd = 4096
+
+#sending window parameters
+snd_ini = 1 #Initial seq number
+snd_mss = 1024
+snd_wnd = snd_mss #sending window size for control use
+
+tx_retransmit_timeout_counter = {}  # Keeps track of the timestamps for the sent out packets, used to determine when to retransmit packet
+rx_retransmit_request_counter = {}  # Keeps track of us sending 'fast retransmit request' packets so we can limit their count to 2
+
+event_connect = threading.Semaphore(0)  # Used to inform CONNECT syscall that connection related event happened
+event_rx_buffer = threading.Semaphore(0)  # USed to inform RECV syscall that there is new data in buffer ready to be picked up
+lock_fsm = threading.RLock()  # Used to ensure that only single event can run FSM at given time
+lock_rx_buffer = threading.Lock()  # Used to ensure only single event has access to RX buffer at given time
+lock_tx_buffer = threading.Lock()  # Used to ensure only single event has access to TX buffer at given time
+
+closing = False  # Indicates that CLOSE syscall is in progress, this lets to finish sending data before FIN packet is transmitted
+ooo_packet_queue = {}  # Out of order packet buffer
+
+
+fsmstate = "CLOSED" #initially the connection is closed, 
+# "SND_CONNECTED" for a harf connection from the sender to the receiver (i.e., sender can send data to the receiver)
+# "CONNECTED" for a full connection between the sender and the receiver
+
 def connection_coontrol_set():
 	print ("1 for reiable connection and 0 for connectionless")
 	ans = input()
@@ -18,9 +54,9 @@ def connection_coontrol_set():
 
 
 
-def packetgeneration(packettype,controltype,data):
+# def packetgeneration(packettype,controltype,data):
 
-	return bytes
+# 	return bytes
 
 
 def ConnectionCreate(connectiontype, address):
@@ -37,3 +73,9 @@ def Data_transmission(controltype, address):
 def connection_close(connectiontype, address):
 
 	return True
+
+
+
+def transmit_data():
+#send out data segment from TX buffer using sliding window mechanism
+	
