931089cf6bf6f37a65e57df33633729ac744d68d
puyanguvic <npuyangpu@gmail.com>
2021-04-02 23:13:03 -0700

    add checksum

--- src/edpsender_socket.py
+++ src/edpsender_socket.py
@@ -361,23 +361,4 @@ class edpsender_socket(self):
 
 
 
-    @staticmethod
-    def checksum(source_string):
-        my_sum = 0
-        count_to = (len(source_string) / 2) * 2
-        count = 0
-        while count < count_to:
-            this_val = ord(source_string[count + 1])*256+ord(source_string[count])
-            my_sum += this_val
-            count += 2
-        if count_to < len(source_string):
-            my_sum += ord(source_string[len(source_string) - 1])
-        my_sum = (my_sum >> 16) + (my_sum & 0xffff)
-        my_sum += (my_sum >> 16)
-        answer = ~my_sum
-        answer = answer & 0xffff
-        answer = answer >> 8 | (answer << 8 & 0xff00)
-        return answer
-	
-
 
