d0f00b41399fc1646e49817612f4e4fc7382fa85
Zhiming-Huang <huangzhiming@mail.nwpu.edu.cn>
2021-04-02 22:47:31 -0700

    update

--- src/edpsender_socket.py
+++ src/edpsender_socket.py
@@ -271,7 +271,7 @@ class edpsender_socket(self):
 			self._transmit_data()
 			return
 
-		if package and packet_type & 0b001: #receive ACK
+		if packet and packet_type & 0b001: #receive ACK
 			if self.snd_una <=packet.ack<=self.snd_max:
 				self._process_ack_packet(packet)
 				if packet.ack >= self.snd_fin:
@@ -358,26 +358,6 @@ class edpsender_socket(self):
     		packet = edppacket(1.0, None)
     		packet.bytes2packet(data_stream)
     		self.edp_fsm(packet=packet)
-
-
-
-    @staticmethod
-    def checksum(source_string):
-        my_sum = 0
-        count_to = (len(source_string) / 2) * 2
-        count = 0
-        while count < count_to:
-            this_val = ord(source_string[count + 1])*256+ord(source_string[count])
-            my_sum += this_val
-            count += 2
-        if count_to < len(source_string):
-            my_sum += ord(source_string[len(source_string) - 1])
-        my_sum = (my_sum >> 16) + (my_sum & 0xffff)
-        my_sum += (my_sum >> 16)
-        answer = ~my_sum
-        answer = answer & 0xffff
-        answer = answer >> 8 | (answer << 8 & 0xff00)
-        return answer
 	
 
 
